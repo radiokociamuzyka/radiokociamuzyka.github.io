eef98e2b363a1072a8f632c41b5b0daa781936f8
Sett Sarverott <sett@sarverott.com>
2026-07-12 12:00:57 +0200

    making risky formating routines, adding autoversioning on pushing

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,34 +1,30 @@
-import type { APIContext } from 'astro';
-import rss from '@astrojs/rss';
+import rss from "@astrojs/rss";
 import { Octokit } from "@octokit/core";
+import type { APIContext } from "astro";
 
 const ghHook = new Octokit({});
 
 const orgInfo = await ghHook.request("GET /orgs/{org}", {
-  org: "radiokociamuzyka"
+	org: "radiokociamuzyka",
 });
-const ghZen = await ghHook.request("GET /zen", {
-
-});
-const ghEvents = await ghHook.request("GET /orgs/{org}/events", {
-  org: "radiokociamuzyka"
+const ghZen = await ghHook.request("GET /zen", {});
+const _ghEvents = await ghHook.request("GET /orgs/{org}/repos", {
+	org: "radiokociamuzyka",
 });
 
-
-
-export function GET(context: APIContext) {
-  return rss({
-    // `<title>` field in output xml
-    title: orgInfo.data.login,
-    // `<description>` field in output xml
-    description: `${orgInfo.data.description}`,
-    // Pull in your project "site" from the endpoint context
-    // https://docs.astro.build/en/reference/api-reference/#site
-    site: `https://radiokociamuzyka.github.io/`,
-    // Array of `<item>`s in output xml
-    // See "Generating items" section for examples using content collections and glob imports
-    items: [],
-    // (optional) inject custom xml
-    customData: `<zen>${ghZen.data}</zen>`,
-  });
+export function GET(_context: APIContext) {
+	return rss({
+		// `<title>` field in output xml
+		title: orgInfo.data.login,
+		// `<description>` field in output xml
+		description: `${orgInfo.data.description}`,
+		// Pull in your project "site" from the endpoint context
+		// https://docs.astro.build/en/reference/api-reference/#site
+		site: `https://radiokociamuzyka.github.io/`,
+		// Array of `<item>`s in output xml
+		// See "Generating items" section for examples using content collections and glob imports
+		items: [],
+		// (optional) inject custom xml
+		customData: `<zen>${ghZen.data}</zen>`,
+	});
 }
